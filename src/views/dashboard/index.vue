c67770afb60b5b6925b549b4560f67a92692a785
yifan <tyf528681034@mail.ustc.edu.cn>
2023-06-20 20:43:01 +0800

    fix delete student button

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
-    <ul v-if="students">
-      <li v-for="student in students" :key="student.id">
-        {{ student.id }}
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import axios from 'axios'
 
 export default {
   name: 'Dashboard',
@@ -19,20 +13,6 @@ export default {
     ...mapGetters([
       'name'
     ])
-  },
-  data() {
-    return {
-      students: null
-    }
-  },
-  async mounted() {
-    try {
-      const response = await axios.get('/api/students') // 注意这里的 URL
-      console.log('Response data:', response.data); // 打印响应数据
-      this.students = response.data;
-    } catch (error) {
-      console.error('Error fetching students:', error);
-    }
   }
 }
 </script>
--- src/views/student/list.vue
+++ src/views/student/list.vue
@@ -14,7 +14,7 @@
       </el-input>
     </div>
     <el-table :data="filteredStudents" border>
-      <el-table-column label="学号" prop="id" header-align="center"></el-table-column>
+      <el-table-column label="学号" prop="student_id" header-align="center"></el-table-column>
       <el-table-column label="姓名" prop="name" header-align="center"></el-table-column>
       <el-table-column label="班级" prop="class" header-align="center"></el-table-column>
       <el-table-column label="专业" prop="major" header-align="center"></el-table-column>
@@ -52,7 +52,7 @@
     >
       <el-form :model="editDialogForm" label-width="80px">
         <el-form-item label="学号">
-          <el-input v-model="editDialogForm.id"></el-input>
+          <el-input v-model="editDialogForm.student_id" disabled></el-input>
         </el-form-item>
         <el-form-item label="姓名">
           <el-input v-model="editDialogForm.name"></el-input>
@@ -64,7 +64,7 @@
           <el-input v-model="editDialogForm.major"></el-input>
         </el-form-item>
         <el-form-item label="院系">
-          <el-input v-model="editDialogForm.department"></el-input>
+          <el-input v-model="editDialogForm.college"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -78,15 +78,6 @@
 <script>
 import axios from 'axios'
 export default {
-  async mounted() {
-    try {
-      const response = await axios.get('/api/students')
-      this.students = response.data;
-      this.filteredStudents = [...this.students]; // 设置 filteredStudents 的初始值
-    } catch (error) {
-      console.error('Error fetching students:', error)
-    }
-  },
   data() {
     return {
       searchQuery: '',
@@ -96,29 +87,37 @@ export default {
       editDialogVisible: false,
       selectedStudent: null,
       editDialogForm: {
-        id: '',
+        student_id: '',
         name: '',
         class: '',
         major: '',
-        department: ''
+        college: ''
       }
-    };
+    }
   },
-  created() {
-    // 初始化 filteredStudents 为所有学生
-    this.filteredStudents = [...this.students];
+  async mounted() {
+    await this.fetchStudents()
   },
   methods: {
+    async fetchStudents() {
+      try {
+        const response = await axios.get('/api/students')
+        this.students = response.data
+        this.filteredStudents = [...this.students]
+      } catch (error) {
+        console.error('Error fetching students:', error)
+      }
+    },
     filterStudents() {
       if (this.searchQuery.trim() === '') {
-        this.filteredStudents = [...this.students];
+        this.filteredStudents = [...this.students]
       } else {
-        const query = this.searchQuery.toLowerCase();
+        const query = this.searchQuery.toLowerCase()
         this.filteredStudents = this.students.filter(
           student =>
-            student.id.toLowerCase().includes(query) ||
+            student.student_id.toLowerCase().includes(query) ||
             student.name.toLowerCase().includes(query)
-        );
+        )
       }
     },
     editStudent(student) {
@@ -130,48 +129,93 @@ export default {
       this.selectedStudent = student
       this.deleteDialogVisible = true
     },
-    deleteStudentConfirm() {
+    async deleteStudentConfirm() {
       if (this.selectedStudent) {
-        const index = this.students.indexOf(this.selectedStudent);
-        if (index > -1) {
-          this.students.splice(index, 1)
-          console.log('删除学生', this.selectedStudent)
+        try {
+          // 发送 DELETE 请求到后端以删除选定的学生
+          const response = await axios.delete(`http://localhost:5000/students/${this.selectedStudent.student_id}`);
+
+          // 检查响应是否表示成功
+          if (response.data.message) {
+            // 从本地数据中删除学生并更新视图
+            const index = this.students.findIndex(student => student.student_id === this.selectedStudent.student_id);
+            if (index > -1) {
+              this.students.splice(index, 1)
+              this.filteredStudents = [...this.students]
+            }
+            this.$message({
+              message: '学生删除成功',
+              type: 'success'
+            })
+          }
+        } catch (error) {
+          console.error('Error deleting student:', error)
+          this.$message({
+            message: '删除学生失败',
+            type: 'error'
+          })
         }
       }
-      this.deleteDialogVisible = false;
-      this.selectedStudent = null;
+      this.deleteDialogVisible = false
+      this.selectedStudent = null
     },
     handleDeleteDialogClose(done) {
       if (this.deleteDialogVisible) {
-        this.deleteDialogVisible = false;
-        this.selectedStudent = null;
+        this.deleteDialogVisible = false
+        this.selectedStudent = null
       }
-      done();
+      done()
     },
-    editStudentConfirm() {
+    async editStudentConfirm() {
       if (this.selectedStudent) {
-        const index = this.students.indexOf(this.selectedStudent);
-        if (index > -1) {
-          this.students.splice(index, 1, { ...this.editDialogForm }); // Update the student with the edited values
-          console.log('编辑学生信息', this.editDialogForm);
+        try {
+          // 创建一个包含所需字段的新对象
+          const dataToUpdate = {
+            name: this.editDialogForm.name,
+            class: this.editDialogForm.class,
+            major: this.editDialogForm.major,
+            college: this.editDialogForm.college
+          }
+          const response = await axios.put(
+            'http://localhost:5000/students/' + this.selectedStudent.student_id,
+            dataToUpdate, // 发送新的对象而不是整个 editDialogForm
+            {
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            }
+          )
+          if (response.data.message) {
+            await this.fetchStudents()
+            this.$message({
+              message: '学生信息更新成功',
+              type: 'success'
+            })
+          }
+        } catch (error) {
+          console.error('Error updating student:', error)
+          this.$message({
+            message: '更新学生信息失败',
+            type: 'error'
+          })
         }
       }
-      this.editDialogVisible = false;
-      this.selectedStudent = null;
+      this.editDialogVisible = false
+      this.selectedStudent = null
     },
     handleEditDialogClose(done) {
       if (this.editDialogVisible) {
-        this.editDialogVisible = false;
-        this.selectedStudent = null;
+        this.editDialogVisible = false
+        this.selectedStudent = null
         this.editDialogForm = {
-          id: '',
+          student_id: '',
           name: '',
           class: '',
           major: '',
           college: ''
-        };
+        }
       }
-      done();
+      done()
     }
   }
 };
